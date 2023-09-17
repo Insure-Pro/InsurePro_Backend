c6f8de8f1f557c03682aac0b39d20b8073ec8f1d
gabang2 <offbeat1020@naver.com>
2023-09-17 16:28:05 +0900

    chore : 고객 생성시 계약 체결 여부도 설정 가능하도록

--- src/main/java/ga/backend/customer/dto/CustomerRequestDto.java
+++ src/main/java/ga/backend/customer/dto/CustomerRequestDto.java
@@ -24,6 +24,7 @@ public class CustomerRequestDto {
                 message = "전화번호 형태는 010-1234-1234 입니다.")
         private String phone; // 연락처
         private String memo; // 메모
+        private Boolean contractYn;
         private LocalDate intensiveCareStartDate; // 집중관리시기 - 시작
         private LocalDate intensiveCareFinishDate; // 집중관리시기 - 끝
         private LocalDate registerDate; // 고객 등록 날짜
