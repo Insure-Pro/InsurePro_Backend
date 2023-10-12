9e01140d07f06a08bb31ad98109650425d000de4
kung036 <sinariari3@gmail.com>
2023-10-12 12:56:59 +0900

    feat : analysis 구현 - 성과분석에 대한 다양한 경우의 수 구현
    issue #75

--- src/main/java/ga/backend/analysis/repository/AnanlysisRepository.java
+++ src/main/java/ga/backend/analysis/repository/AnanlysisRepository.java
@@ -1,6 +1,7 @@
 package ga.backend.analysis.repository;
 
 import ga.backend.analysis.entity.Analysis;
+import ga.backend.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,5 @@ import java.util.Optional;
 
 @Repository
 public interface AnanlysisRepository extends JpaRepository<Analysis, Long> {
-    Optional<Analysis> findByDate(LocalDate localDate);
+    Optional<Analysis> findByEmployeeAndDate(Employee employee, LocalDate localDate);
 }
