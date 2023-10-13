c87e84c6953b2ae58e560a4748261560ecf4c416
kung036 <sinariari3@gmail.com>
2023-10-13 19:04:50 +0900

    feat : delYn 추가 - cutomer 월별 필터링에 추가
    issue #56

--- src/main/java/ga/backend/customer/repository/CustomerRepository.java
+++ src/main/java/ga/backend/customer/repository/CustomerRepository.java
@@ -14,34 +14,28 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     // CustomerService
-    List<Customer> findAllByEmployee(Employee employee, Sort sort);
+    Optional<Customer> findByPkAndDelYnFalse(long customerPk);
+
+
+    List<Customer> findByEmployeeAndDelYnFalse(Employee employee, Sort sort);
     List<Customer> findAllByEmployeeAndCreatedAtBetween(Employee employee, Sort sort, LocalDateTime start, LocalDateTime finish);
 
-    List<Customer> findByEmployeeAndAgeBetween(Employee employee, int startAge, int endAge, Sort sort);
-    List<Customer> findByEmployeeAndAgeBetweenAndCreatedAtBetween(Employee employee, int startAge, int endAge, Sort sort, LocalDateTime start, LocalDateTime finish);
 
-    List<Customer> findByEmployeeAndDongStringContains(Employee employee, String dongName, Sort sort);
-    List<Customer> findByEmployeeAndDongStringContainsAndCreatedAtBetween(Employee employee, String dongName, Sort sort, LocalDateTime start, LocalDateTime finish);
-    List<Customer> findByEmployeeAndContractYn(Employee employee, boolean contractYn);
-    List<Customer> findByEmployeeAndContractYnAndCreatedAtBetween(Employee employee, boolean contractYn,  LocalDateTime start, LocalDateTime finish);
+    List<Customer> findByEmployeeAndAgeBetweenAndDelYnFalse(Employee employee, int start, int end, Sort sort);
+    List<Customer> findByEmployeeAndAgeBetweenAndCreatedAtBetweenAndDelYnFalse(Employee employee, int startAge, int endAge, Sort sort, LocalDateTime start, LocalDateTime finish);
+
+
+    List<Customer> findByEmployeeAndDongStringContainsAndDelYnFalse(Employee employee, String dongName, Sort sort);
+    List<Customer> findByEmployeeAndDongStringContainsAndCreatedAtBetweenAndDelYnFalse(Employee employee, String dongName, Sort sort, LocalDateTime start, LocalDateTime finish);
+
+    List<Customer> findByEmployeeAndContractYnAndDelYnFalse(Employee employee, boolean contractYn);
+    List<Customer> findByEmployeeAndContractYnAndCreatedAtBetweenAndDelYnFalse(Employee employee, boolean contractYn, LocalDateTime start, LocalDateTime finish);
+
+
     List<Customer> findByEmployeeAndName(Employee employee, String name);
 
     // -----------------------------------------------------------------------------
     // 성과분석(analysis)
-    Optional<Customer> findByPkAndDelYnFalse(long customerPk);
-
-    List<Customer> findByEmployeeAndDelYnFalse(Employee employee, Sort sort);
-
-    List<Customer> findByEmployeeAndAgeBetweenAndDelYnFalse(Employee employee, int start, int end, Sort sort);
-
-    List<Customer> findByEmployeeAndDongStringContainsAndDelYnFalse(Employee employee, String dongName, Sort sort);
-
-    List<Customer> findByEmployeeAndContractYnAndDelYnFalse(Employee employee, boolean contractYn);
-
-    @Query("SELECT c FROM Customer c " +
-            "WHERE (c.employee = :employee AND c.delYn = false AND (c.intensiveCareStartDate != NULL OR c.intensiveCareFinishDate != NULL))")
-    List<Customer> findByEmployeeAndIntensiveCareExists(Employee employee);
-
     // all 계산
     List<Customer> findByEmployeeAndCreatedAtBetweenAndDelYnFalse(
             Employee employee,
