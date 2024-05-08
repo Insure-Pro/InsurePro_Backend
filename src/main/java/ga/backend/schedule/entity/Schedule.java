4f58a8e63cfdd877fcaf332b1db86d668e4a6da9
kung036 <sinariari3@gmail.com>
2024-05-08 11:32:01 +0900

    feat : Schedule의 progress 값 추가
    - IC : 정보수집(Information Collaction)
    - OC : 기타상담(Other consultation)
    - CIC : 계약체결(Conclusion of an Insurance Contract)
    
    issue #163

--- src/main/java/ga/backend/schedule/entity/Schedule.java
+++ src/main/java/ga/backend/schedule/entity/Schedule.java
@@ -56,7 +56,10 @@ public class Schedule extends Auditable {
         // 초회상담(AP) / 상품제안(PC) / 증권전달(ST)
         AP("초회상담"),
         PC("상품제안"),
-        ST("증권전달");
+        ST("증권전달"),
+        IC("정보수집"),
+        OC("기타상담"),
+        CIC("계약체결");
 
         private final String value;
 
