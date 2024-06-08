329d828253d9c36abafa986298743a9f409eb537
kung036 <sinariari3@gmail.com>
2024-06-08 21:35:42 +0900

    feat : default 고객유형 customerType 생성
    - customerType의 default인 NULL 고객유형 생성하는 메서드 정의
    
    issue #178

---
+++ src/main/java/ga/backend/util/InitialCustomerTypeNull.java
@@ -0,0 +1,18 @@
+package ga.backend.util;
+
+import ga.backend.customerType.entity.CustomerType;
+import ga.backend.customerType.entity.DataType;
+import ga.backend.employee.entity.Employee;
+
+public class InitialCustomerTypeNull {
+    static public CustomerType makeCustomerType(Employee employee) {
+        CustomerType customerType = new CustomerType();
+        customerType.setCompany(employee.getCompany());
+        customerType.setEmployeePk(employee.getPk());
+        customerType.setName("NULL");
+        customerType.setDetail("customerType을 지정하지 않을 경우 사용하는 default값");
+        customerType.setDataType(DataType.ETC);
+        customerType.setColor("#000000");
+        return customerType;
+    }
+}
