de4d6d15fcc03a6782c2d8bc830adba85fb38b86
kung036 <sinariari3@gmail.com>
2024-08-19 20:27:40 +0900

    docs : 이메일 관련 환경변수 수정
    - GlobalExceptionHandler과 BusinessLogicException이 아닌 모든 ERROR가 발생했을 때 로그 파일이 만들어지도록 수정
    
    issue #185

--- src/main/java/ga/backend/log/ExceptionFilter.java
+++ src/main/java/ga/backend/log/ExceptionFilter.java
@@ -1,5 +1,6 @@
 package ga.backend.log;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.StackTraceElementProxy;
 import ch.qos.logback.core.spi.FilterReply;
@@ -8,8 +9,8 @@ import ch.qos.logback.core.filter.Filter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
+import java.util.Map;
 import java.util.TimeZone;
 
 public class ExceptionFilter extends Filter<ILoggingEvent> {
@@ -25,38 +26,18 @@ public class ExceptionFilter extends Filter<ILoggingEvent> {
             securityLogEvent = event;
         }
 
-        // BusinessLogicException일 경우 -> Requset 내용 추가
-        if (logger.contains("BusinessLogicException")) {
-            // 저장된 "Spring Security Debugger" 로그 출력
-            if (securityLogEvent != null) {
-                String message = securityLogEvent.getMessage();
-                message = message.split("Security filter chain")[0];
-                message += "************************************************************";
+        // 모든 ERROR 로그를 파일에 기록하도록 설정
+        if (event.getLevel().isGreaterOrEqual(Level.ERROR)) {
+            writeSecurityLogToFile();
 
-                // 파일에 로그 메시지를 기록
-                writeLogToFile(message);
-
-                // securityLogEvent를 null로 설정해 중복 기록 방지
-                securityLogEvent = null;
-            }
-
-            return FilterReply.ACCEPT;
-        }
-
-        // GlobalExceptionHandler일 경우 -> 내용 요약하기
-        if (logger.contains("GlobalExceptionHandler")) {
-            // 전체 메시지 가져오기
             String message = event.getMessage();
-
-            // Throwable이 있는 경우, 스택 트레이스를 가져와서 메시지 추가
             if (event.getThrowableProxy() != null) {
                 StringBuilder sb = new StringBuilder();
                 sb.append(message).append(System.lineSeparator());
 
                 // 예외 스택 트레이스 추가
                 for (StackTraceElementProxy line : event.getThrowableProxy().getStackTraceElementProxyArray()) {
-                    sb.append(line).append(System.lineSeparator());
-                    // 메시지 길이를 제한하여 생략 처리
+                    sb.append("   ").append(line).append(System.lineSeparator());
                     if (sb.length() > 1000) {
                         sb.append("... [truncated]");
                         break;
@@ -101,12 +82,27 @@ public class ExceptionFilter extends Filter<ILoggingEvent> {
         return String.format("%s  %-5s --- [%s] %s : %s", timestamp, level, threadName, loggerName, message);
     }
 
-
     // 파일명 설정
     private String getFormattedFileName() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm");
         sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));  // 시간대를 Asia/Seoul로 설정
         String dateTime = sdf.format(new Date());
         return String.format(fileNamePattern, dateTime);
+    }
+
+    // Spring Security Debugger 출력  -> Requset 내용 추가
+    private void writeSecurityLogToFile() {
+        // 저장된 "Spring Security Debugger" 로그 출력
+        if (securityLogEvent != null) {
+            String message = securityLogEvent.getMessage();
+            message = message.split("Security filter chain")[0];
+            message += "************************************************************";
+
+            // 파일에 로그 메시지를 기록
+            writeLogToFile(message);
+
+            // securityLogEvent를 null로 설정해 중복 기록 방지
+            securityLogEvent = null;
+        }
     }
 }
